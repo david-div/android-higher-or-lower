7a34254d589dbbcf809fd496b998d7ae0d342f1d
David Divilly <d.divilly@gmail.com>
2017-12-01 21:11:44 +0000

    Added string and int to input number

--- app/src/main/java/com/example/dddivilly/higherorlower/MainActivity.java
+++ app/src/main/java/com/example/dddivilly/higherorlower/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.*;
 
@@ -15,9 +16,17 @@ public class MainActivity extends AppCompatActivity {
 
         int randomNumber = rand.nextInt(20) + 1;
 
-        EditText number = (EditText) findViewById(R.id.numberInput);
 
-        System.out.println(number.getText().toString());
+        EditText numberInput = (EditText) findViewById(R.id.numberInput);
+
+        String value = numberInput.getText().toString();
+
+        int number = Integer.parseInt(value);
+
+
+        Toast.makeText(MainActivity.this, "Toasts working", Toast.LENGTH_LONG).show();
+
+        System.out.println(numberInput.getText().toString());
         System.out.println(Integer.toString(randomNumber));
 
     }
