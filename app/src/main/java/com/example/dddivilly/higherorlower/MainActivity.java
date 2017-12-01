a033e6c7ae3d64f509e936bc112309ba3c6e380a
David Divilly <d.divilly@gmail.com>
2017-12-01 20:56:11 +0000

    Added random generator

--- app/src/main/java/com/example/dddivilly/higherorlower/MainActivity.java
+++ app/src/main/java/com/example/dddivilly/higherorlower/MainActivity.java
@@ -5,13 +5,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import java.util.*;
+
 public class MainActivity extends AppCompatActivity {
 
     public void guess(View view){
 
+        Random rand = new Random();
+
+        int randomNumber = rand.nextInt(20) + 1;
+
         EditText number = (EditText) findViewById(R.id.numberInput);
 
         System.out.println(number.getText().toString());
+        System.out.println(Integer.toString(randomNumber));
 
     }
 
