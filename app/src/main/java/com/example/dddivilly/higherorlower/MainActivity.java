42c5e02af5c11b4199ecce6164cf20c3a38d3e9e
David Divilly <d.divilly@gmail.com>
2017-12-01 20:31:41 +0000

    Added ID and hint

--- app/src/main/java/com/example/dddivilly/higherorlower/MainActivity.java
+++ app/src/main/java/com/example/dddivilly/higherorlower/MainActivity.java
@@ -2,8 +2,18 @@ package com.example.dddivilly.higherorlower;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void guess(View view){
+
+        EditText number = (EditText) findViewById(R.id.numberInput);
+
+
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
